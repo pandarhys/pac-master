d6ec2394e568f9b71d1538ca94c4e67485e93191
DESKTOP-JLQ9L9P\Rhys-2020-PC <rhyswilliams1989@gmail.com>
2021-02-16 16:49:15 +0000

    changes to docker php nginx to accept large file size

--- php.dockerfile
+++ php.dockerfile
@@ -1,5 +1,10 @@
 FROM php:7.4.12-fpm-alpine
 
+RUN cp /usr/local/etc/php/php.ini-production /usr/local/etc/php/php.ini && \
+        sed -i -e "s/^ *memory_limit.*/memory_limit = 4G/g" /usr/local/etc/php/php.ini && \
+        sed -i -e "s/^ *upload_max_filesize .*/upload_max_filesize  = 4G/g" /usr/local/etc/php/php.ini && \
+        sed -i -e "s/^ *post_max_size .*/post_max_size  = 4G/g" /usr/local/etc/php/php.ini
+
 RUN mkdir -p /var/www/html
 
 RUN apk --no-cache add shadow && usermod -u 1000 www-data
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <main class="lg:flex lg:flex-wrap -mx-1">
         <div id="welcome">
             <h1>
-                Welcome to the People's Archive of Costume TEST
+                Welcome to the People's Archive of Costume
             </h1>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eleifend nisl nisl, nec bibendum eros eleifend ac. Donec dui nunc, tincidunt sed nibh vel, tincidunt vestibulum nulla. Nam id ante blandit mauris ultrices malesuada. Nulla facilisi. Aliquam commodo blandit nibh nec fringilla. Suspendisse potenti. Fusce posuere euismod urna, sed suscipit magna euismod quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean scelerisque mattis tellus ac scelerisque. Praesent posuere turpis sit amet vulputate ultrices. Aenean interdum lorem sed viverra rhoncus. Donec rhoncus, dolor et feugiat porttitor, lacus nibh pharetra nisi, ultricies accumsan sem lacus placerat enim. Sed non magna a nibh efficitur iaculis.
